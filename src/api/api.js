6a8a82e321b193ddc648e5bb4d3a68db8aae9209
Omkar Pawar <pawarnandkumaromved@gmail.com>
2026-02-15 12:41:39 +0530

    Update API base URL to use environment variable

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// ⭐ ALWAYS attach token before every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
